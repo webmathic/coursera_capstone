f516f0ba7483ecc541640a781c42ea38862cec50
Taiwo <taiwoajibola01@gmail.com>
2020-06-29 22:01:39 +0100

    python hello world

---
+++ capstone_project.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# Capstone Project
+
+# In[1]:
+
+
+import pandas as pd
+import numpy as numpy
+
+
+# In[2]:
+
+
+print("Hello Capstone Project Course")
+
+
+# In[ ]:
+
+
+
+
